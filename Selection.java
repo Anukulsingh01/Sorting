1994e37d85c665ad85ed40c42dfbeb6d7675a8a9
Anukulsingh01 <anukulsinghbisht@gmail.com>
2025-02-14 19:46:54 +0530

    codes for both Selection and bubble sort are here

--- Selection.java
+++ Selection.java
@@ -1,5 +1,34 @@
 package Sorting;
 
-public class BubbleSort {
+import java.util.Arrays;
+
+public class Selection {
+        public static void main(String[] args) {
+        int[] arr={12,34,5,6,78,9};
+        Selection(arr);
+       System.out.println(Arrays.toString(arr));
+
+    }
+    static void Selection(int[]arr){
+        for (int i=0;i<arr.length;i++){
+            int last= arr.length-i-1;
+            int maxIndex= getMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+    static void swap(int []arr,int First,int Second){
+            int temp =arr[First];
+            arr[First]=arr[Second];
+            arr[Second]=temp;
+    }
+     static int getMaxIndex(int[]arr,int start,int end){
+        int max=start;
+        for(int i=start;i<=end;i++){
+          if(arr[max]<arr[i]){
+            max=i;
+          }
+        }
+        return max;
+    }
     
 }
